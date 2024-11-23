cd68432afc4806eba33f803f5c3d710d6a2e473a
Artem <lunycat@mail.ru>
2024-11-23 11:57:13 +0300

    correct address DB

--- app/jte-classes/gg/jte/generated/ondemand/urls/JteshowGenerated.java
+++ app/jte-classes/gg/jte/generated/ondemand/urls/JteshowGenerated.java
@@ -11,7 +11,7 @@ public final class JteshowGenerated {
 				jteOutput.writeContent("\r\n    <main class=\"flex-grow-1\">\r\n\r\n        <section>\r\n\r\n            <div class=\"container-lg mt-5\">\r\n                <h1>Сайт: ");
 				jteOutput.setContext("h1", null);
 				jteOutput.writeUserContent(page.getUrl().getName());
-				jteOutput.writeContent("</h1>\r\n\r\n                <table class=\"table table-bordered table-hover mt-3\">\r\n                    <tbody>\r\n                    <tr>\r\n                        <td>ID</td>\r\n                        <td>");
+				jteOutput.writeContent("</h1>\r\n\r\n                <table class=\"table table-bordered border-primary\">\r\n                    <tbody>\r\n                    <tr>\r\n                        <td>ID</td>\r\n                        <td>");
 				jteOutput.setContext("td", null);
 				jteOutput.writeUserContent(page.getUrl().getId());
 				jteOutput.writeContent("</td>\r\n                    </tr>\r\n                    <tr>\r\n                        <td>Имя</td>\r\n                        <td>");
@@ -20,7 +20,7 @@ public final class JteshowGenerated {
 				jteOutput.writeContent("</td>\r\n                    </tr>\r\n                    <tr>\r\n                        <td>Дата создания</td>\r\n                        <td>");
 				jteOutput.setContext("td", null);
 				jteOutput.writeUserContent(page.getUrl().getCreatedAt().toString());
-				jteOutput.writeContent("</td>\r\n                    </tr>\r\n                    </tbody>\r\n                </table>\r\n\r\n                <h2 class=\"mt-5\">Проверки</h2>\r\n                <form method=\"post\" action=\"/urls/1/checks\">\r\n                    <button type=\"submit\" class=\"btn btn-primary\">Запустить проверку</button>\r\n                </form>\r\n\r\n                <table class=\"table table-bordered table-hover mt-3\">\r\n                    <thead>\r\n                    <th class=\"col-1\">ID</th>\r\n                    <th class=\"col-1\">Код ответа</th>\r\n                    <th>title</th>\r\n                    <th>h1</th>\r\n                    <th>description</th>\r\n                    <th class=\"col-2\">Дата проверки</th>\r\n                    </thead>\r\n                    <tbody>\r\n\r\n                    </tbody>\r\n                </table>\r\n            </div>\r\n\r\n        </section>\r\n    </main>\r\n");
+				jteOutput.writeContent("</td>\r\n                    </tr>\r\n                    </tbody>\r\n                </table>\r\n\r\n                <h2 class=\"mt-5\">Проверки</h2>\r\n                <form method=\"post\" action=\"/urls/1/checks\">\r\n                    <button type=\"submit\" class=\"btn btn-primary\">Запустить проверку</button>\r\n                </form>\r\n                <section></section>\r\n                <table class=\"table table-bordered border-primary\">\r\n                    <thead>\r\n                    <th class=\"col-1\">ID</th>\r\n                    <th class=\"col-1\">Код ответа</th>\r\n                    <th>title</th>\r\n                    <th>h1</th>\r\n                    <th>description</th>\r\n                    <th class=\"col-2\">Дата проверки</th>\r\n                    </thead>\r\n                    <tbody>\r\n\r\n                    </tbody>\r\n                </table>\r\n            </div>\r\n\r\n        </section>\r\n    </main>\r\n");
 			}
 		});
 	}
--- app/src/main/java/hexlet/code/util/Utils.java
+++ app/src/main/java/hexlet/code/util/Utils.java
@@ -17,7 +17,9 @@ import java.util.stream.Collectors;
 public class Utils {
 
     public static int getPort() {
-        String port = System.getenv().getOrDefault("PORT", "7070");
+        String port = System
+                .getenv()
+                .getOrDefault("PORT", "7070");
         return Integer.parseInt(port);
     }
 
